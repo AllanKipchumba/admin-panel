1bce0979921a5b9b29e0970148b3403b9257d85a
_Allan <allankipchumba76@gmail.com>
2022-06-19 13:03:06 +0300

    partially completed featured component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,6 +3,8 @@ import "./featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const Featured = () => {
   return (
@@ -15,6 +17,34 @@ const Featured = () => {
         <div className="bottom">
           <div>
             <CircularProgressbar value={60} text="70%" strokeWidth={5} />
+          </div>
+          <p>Total sales made today</p>
+          <p>$1000</p>
+          <p>
+            Previous transactions processing. Last payements may not be
+            included.
+          </p>
+          <div className="target">
+            <div className="negative">
+              <p>target</p>
+              <div>
+                <KeyboardArrowDownIcon />
+                <p>$20.3</p>
+              </div>
+            </div>
+            <div>
+              <p>last week</p>
+              <div>
+                <KeyboardArrowUpIcon /> <p>$20.3</p>
+              </div>
+            </div>
+            <div>
+              <p>last month</p>
+              <div>
+                <KeyboardArrowUpIcon />
+                <p>$20.3</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
