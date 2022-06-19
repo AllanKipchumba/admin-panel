c850954562cfd74a86d51367160c14d223232c85
_Allan <allankipchumba76@gmail.com>
2022-06-19 09:14:19 +0300

    completed widget component

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./widget.scss";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
@@ -8,56 +8,57 @@ import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlin
 
 const Widget = ({ type }) => {
   const [data, setData] = useState({
-    title: "user",
-    isMoney: true,
+    title: "",
+    isMoney: Boolean,
     link: "link",
-    icon: <MonetizationOnOutlinedIcon classNameicon />,
+    icon: <PersonOutlineOutlinedIcon />,
   });
 
   //   temporary data
   const amount = 100;
   const diff = 20;
 
-  //   switch (type) {
-  //     case "user":
-  //       data = {
-  //         title: "users",
-  //         isMoney: false,
-  //         link: "see all users",
-  //         icon: <PersonOutlineOutlinedIcon className="icon" />,
-  //       };
-  //       break;
+  useEffect(() => {
+    switch (type) {
+      case "user":
+        setData({
+          title: "users",
+          isMoney: false,
+          link: "see all users",
+          icon: <PersonOutlineOutlinedIcon className="icon" />,
+        });
+        break;
 
-  //     case "order":
-  //       data = {
-  //         title: "orders",
-  //         isMoney: true,
-  //         link: "see all arders",
-  //         icon: <ShoppingCartOutlinedIcon className="icon" />,
-  //       };
-  //       break;
+      case "order":
+        setData({
+          title: "orders",
+          isMoney: true,
+          link: "see all arders",
+          icon: <ShoppingCartOutlinedIcon className="icon" />,
+        });
+        break;
 
-  //     case "earning":
-  //       data = {
-  //         title: "earnings",
-  //         isMoney: true,
-  //         link: "view net earnings",
-  //         icon: <MonetizationOnOutlinedIcon className="icon" />,
-  //       };
-  //       break;
+      case "earnings":
+        setData({
+          title: "earnings",
+          isMoney: true,
+          link: "view net earnings",
+          icon: <MonetizationOnOutlinedIcon className="icon" />,
+        });
+        break;
 
-  //     case "balance":
-  //       data = {
-  //         title: "balance",
-  //         isMoney: true,
-  //         link: "see details",
-  //         icon: <AccountBalanceOutlinedIcon className="icon" />,
-  //       };
-  //       break;
-
-  //     default:
-  //       break;
-  //   }
+      case "balance":
+        setData({
+          title: "balance",
+          isMoney: true,
+          link: "see details",
+          icon: <AccountBalanceOutlinedIcon className="icon" />,
+        });
+        break;
+      default:
+        break;
+    }
+  }, [type]);
 
   return (
     <>
